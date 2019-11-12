7aacdfa4a0032cb6797d207d1286c192f577fad5
hvns08 <52692404+hvns08@users.noreply.github.com>
2019-11-12 14:30:27 +0400

    Add files via upload

---
+++ day 9 - Sealed Methods and Sealed Methods/Sealed/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Sealed
+{
+    class A
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Show");
+        }
+    }
+
+    class B : A
+    {
+        public override void Show()
+        {
+            Console.WriteLine("show");
+
+        }
+    }
+
+    sealed class C : B
+    {
+        //public override sealed void Show()
+        //{
+        //    {
+        //        Console.WriteLine("Show of C");
+        //    }
+        //}
+    }
+
+    class D
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C c = new C();
+            //c.Show();
+
+            D d = new D();
+            d.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
