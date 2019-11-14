b040278819f3f569a7f979c73fe9d64910abb072
hvns08 <52692404+hvns08@users.noreply.github.com>
2019-11-14 10:23:52 +0400

    Add files via upload

---
+++ day 12 - Exception Handling/BLL/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BLL
+{
+    public class Calculate
+    {
+        public static double Divide(int a,int b)
+        {
+            try
+            {
+                return a / b;
+
+            }
+
+            catch(Exception E)
+            {
+                throw new Exception("Invalid Second Param or b value cannot be zero:");
+            }
+
+        }
+    }
+}
---
+++ day 12 - Exception Handling/ExceptionHandling/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using BLL;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] A = new int[2];
+            //try
+            //{
+
+            //    A[0] = 0;
+            //    Console.WriteLine(A[0]);
+
+            //    A[1] = 45;
+            //    Console.WriteLine(A[1]);
+
+            //    var x = A[1] / A[0];
+
+            //    A[2] = 7;
+            //    Console.WriteLine(A[2]);
+            //}
+
+
+
+            //catch (FormatException FE)
+            //{
+
+
+            //    Console.WriteLine(FE.Message);
+
+            //    A[1] = 0;
+            //}
+
+            //catch (DivideByZeroException DZ)
+            //{
+
+
+            //    Console.WriteLine(DZ.Message);
+            //}
+
+            //catch (IndexOutOfRangeException IE)
+            //{
+
+
+            //    Console.WriteLine(IE.Message);
+            //}
+
+            //catch (Exception E)
+            //{
+
+
+            //    Console.WriteLine(E.Message);
+
+            //    A[1] = 0;
+            //}
+
+
+            //finally { }
+            try
+            {
+
+                var result = Calculate.Divide(15, 0);
+                Console.WriteLine("Result is " + result);
+            }
+            catch (Exception E)
+            {
+
+                Console.WriteLine(E.Message);
+            }
+
+            finally
+            {
+                //
+            }
+            Console.ReadLine();
+        }
+    }
+}
